16a95b9ac1952c340dd1e14c203a13ef72d0f038
Bruno Biondi <eu@brunobiondi.com>
2022-11-05 20:20:27 -0300

    Configuration import alias and import SVG. Add styled-components

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
+import alias from '@rollup/plugin-alias'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    svgr(),
+    react(),
+    alias({
+      entries: [
+        {
+          find: '@',
+          replacement: resolve(__dirname, 'src'),
+        },
+        {
+          find: '@public',
+          replacement: resolve(__dirname, 'public'),
+        },
+      ],
+    }),
+  ],
 })
