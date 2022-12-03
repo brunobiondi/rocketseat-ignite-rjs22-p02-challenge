f5587ba4b9898c5698c4519ab74e02416da24026
Bruno Biondi <eu@brunobiondi.com>
2022-12-02 22:27:19 -0300

    feat: delivery form

--- src/components/Checkout/components/Delivery/index.tsx
+++ src/components/Checkout/components/Delivery/index.tsx
@@ -1,5 +1,5 @@
 import { BoxTitle } from '../BoxTitle'
-import { Container } from './styled'
+import { Container, FlexColumn, FlexRow, Form, Input, LabelObs } from './styled'
 
 import { ReactComponent as SvgCheckoutLocation } from '@/assets/checkout-location.svg'
 
@@ -11,6 +11,41 @@ export const Delivery = () => {
         subtitle="Informe o endereço onde deseja receber seu pedido"
         image={<SvgCheckoutLocation />}
       />
+      <Form>
+        <FlexRow>
+          <FlexColumn flex={1}>
+            <Input placeholder="CEP" />
+          </FlexColumn>
+          <FlexColumn flex={2}></FlexColumn>
+        </FlexRow>
+        <FlexRow>
+          <FlexColumn>
+            <Input placeholder="Rua" />
+          </FlexColumn>
+        </FlexRow>
+        <FlexRow>
+          <FlexColumn flex={1}>
+            <Input placeholder="Número" />
+          </FlexColumn>
+          <FlexColumn flex={2}>
+            <LabelObs htmlFor="complement">Opcional</LabelObs>
+            <Input placeholder="Complemento" paddingRight="4.5rem" />
+          </FlexColumn>
+        </FlexRow>
+        <FlexRow>
+          <FlexColumn flex={1}>
+            <Input placeholder="Bairro" />
+          </FlexColumn>
+          <FlexColumn flex={2}>
+            <FlexColumn flex={4}>
+              <Input placeholder="Cidade" />
+            </FlexColumn>
+            <FlexColumn flex={1}>
+              <Input placeholder="UF" />
+            </FlexColumn>
+          </FlexColumn>
+        </FlexRow>
+      </Form>
     </Container>
   )
 }
--- src/components/Checkout/components/Delivery/styled.ts
+++ src/components/Checkout/components/Delivery/styled.ts
@@ -5,3 +5,49 @@ export const Container = styled.div`
   border-radius: 0.375rem;
   padding: 2.5rem;
 `
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`
+
+export const FlexRow = styled.div`
+  display: flex;
+  gap: 0.75rem;
+`
+
+export const FlexColumn = styled.div<{ flex?: number }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: ${({ flex }) => flex ?? 1};
+  gap: 0.75rem;
+
+  position: relative;
+`
+
+export const Input = styled.input.attrs({
+  type: 'text',
+})<{ paddingRight?: string }>`
+  background: ${({ theme }) => theme.colors.base.input};
+  border: 1px solid ${({ theme }) => theme.colors.base.button};
+  border-radius: 0.25rem;
+  color: ${({ theme }) => theme.colors.base.text};
+  padding: 0.75rem ${({ paddingRight }) => paddingRight ?? '0.75rem'} 0.75rem
+    0.75rem;
+  width: 100%;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.base.label};
+    opacity: 1;
+  }
+`
+
+export const LabelObs = styled.label`
+  color: ${({ theme }) => theme.colors.base.label};
+  font-size: 0.75rem;
+  font-style: italic;
+  position: absolute;
+  right: 0.75rem;
+`
