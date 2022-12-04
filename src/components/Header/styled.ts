a16f889e558f8ca1f6a0ae88d1b106562b1eec0a
Bruno Biondi <eu@brunobiondi.com>
2022-12-04 08:15:49 -0300

    style: fixed header

--- src/components/Header/styled.ts
+++ src/components/Header/styled.ts
@@ -1,9 +1,12 @@
 import styled from 'styled-components'
 
 export const Container = styled.header`
+  background: ${({ theme }) => theme.colors.base.background};
   margin: 0 auto;
   max-width: 75rem;
   padding: 2rem 2.5rem;
+  position: sticky;
+  top: 0;
 
   display: flex;
   align-items: center;
