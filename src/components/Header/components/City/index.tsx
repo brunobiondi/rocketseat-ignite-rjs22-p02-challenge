a88bad99932b8abbfc62c9d4f3a65f0778a88de2
Bruno Biondi <eu@brunobiondi.com>
2023-03-14 19:04:35 -0300

    feat: context data

--- src/components/Header/components/City/index.tsx
+++ src/components/Header/components/City/index.tsx
@@ -1,11 +1,15 @@
 import { ReactComponent as IconLocation } from '@/assets/location.svg'
 import { LinkCity } from './styled'
+import { useContext } from 'react'
+import { ShopContext } from '@/contexts/Shop/ShopContext'
+import { Link } from 'react-router-dom'
 
 export const City = () => {
+  const { delivery } = useContext(ShopContext)
   return (
-    <LinkCity>
+    <LinkCity as={Link} to="/checkout">
       <IconLocation />
-      Porto Alegre, RS
+      {delivery.city}, {delivery.state}
     </LinkCity>
   )
 }
